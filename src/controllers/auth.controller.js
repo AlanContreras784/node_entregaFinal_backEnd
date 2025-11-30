4ce22533e1ecab480b04e126727a9ec79d63de81
Alan Contreras <alancontreras74@gmail.com>
2025-11-30 17:29:55 -0300

    probando autenticacion

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,7 +3,7 @@ import { generateToken } from "../config/token.js";
 export const login = async (req, res) => {
     console.log(req.body)
     const { email, password } = req.body;
-    if (email === "test@gmail.com" && password === "123456") {
+    if( (email === "test@gmail.com" && password === "123456") || (email === "admin@gmail.com" && password === "test12") ) {
         const user = {email: email, id: "123"}
         const token = await generateToken(user);
         res.json({ token });
