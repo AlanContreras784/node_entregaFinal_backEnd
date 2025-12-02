1fcc96cf207532a882d6a6326001cba7df55bf12
Alan Contreras <alancontreras74@gmail.com>
2025-12-01 22:10:25 -0300

    prueba

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
 
 //routes
 app.get("/", (req, res) => {
-  res.json({ title: "Home Page" });
+  res.json({ title: "Home Page Bienvenidos" });
 });
 //app.use("/api/users", userRoutes);
 app.use("/api", productsRoutes);
